b5473b10ce491a8d1cad66860007dc0ebdaa1873
giorgi200 <giorgi.kochiashvili06@geolab.edu.ge>
2018-05-19 14:37:48 +0400

    for test with json db

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var socket = require('socket.io');
+var fs = require('fs');
+
+var visitor_names = [ ];
 
 var app = express();
  
@@ -9,22 +12,45 @@ var server = app.listen(4000, function() {
 
 app.use(express.static('public'));
 
+
 var io = socket(server);
-
 io.on('connection', function(socket) {
+
+    socket.on('edit', function(data) {
+        socket.broadcast.emit('edit', data);
+    })
+
+    socket.on('del', function(data) {
+        io.sockets.emit('del', data);
+
+        visitor_names.slice(data.del_id);
+        console.log(visitor_names[data.del_id]);
+    })
+
     socket.on('chat', function(data) {
+        var d = new Date();
+        
+        var key = {
+            id: visitor_names.length + 1,
+            სახელი: data.name,
+            დრო: d.getHours() + ":" + d.getMinutes()
+        }
+
         io.sockets.emit('chat', data);
-    });
+        visitor_names.push(key);
+        io.sockets.emit('lens', {
+            
+            leng:visitor_names.length,
+        
+        });
 
-    socket.on('typing', function(data) {
-        socket.broadcast.emit('typing', data);
-    });
-    socket.on('online', function(data) {
-        socket.broadcast.emit('online', data);
-    });
+        fs.writeFile('./visitors.json', JSON.stringify(visitor_names, null, 4), (err) => {
+           
+            if (err) {
+                console.error(err);
+            }      
+        })
 
-    socket.on('change', function(data) {
-        socket.broadcast.emit('change', data);
     });
-
 })
+
---
+++ public/js/chat.js
@@ -0,0 +1,100 @@
+var x = 0;
+var was = false;
+var socket = io.connect('192.68.1.17:4000');
+
+// Query DOM
+let body = document.querySelector('body');
+let len = document.querySelector('.len');
+
+// მესიჯის input
+var message = document.getElementById('message');
+// პერსონის intput
+var p_input = document.getElementById('p_input');
+// პერსონის chose
+var chose = document.getElementById('chose');
+// პერსონის output
+var p_output = document.getElementById('p_output');
+// მიღებული მესიჯები
+var output = document.getElementById('output');
+
+ 
+p_input.addEventListener('click', function() {
+    chose.style.display = "none";
+    output.className = "in";
+    
+})
+
+p_output.addEventListener('click', function() {
+    chose.style.display = "none";
+    message.style.display = "none";
+    chose.style.display = "none";
+    was = true;
+
+    
+})
+
+// წერილის გაგზავნის ფუნქცია 
+message.addEventListener('keyup', function() {
+    if (event.keyCode == 13 ) {
+        socket.emit('chat', {
+            name: message.value.replace("\n", ""),
+        })
+
+        message.value = "";
+
+    }
+});
+
+// Edit Customer Name
+socket.on('edit', function(data) {
+    var mes = document.querySelectorAll('#output p');
+    mes[data.u_id].innerText = data.input;
+    
+})
+
+// Delete Customer
+socket.on('del', function(data) {
+    output.removeChild(output.childNodes[data.del_id]);    
+})
+// Customers Length 
+socket.on('lens', function(data) {
+    len.innerText = data.leng;  
+})
+
+// Add Customer Name
+socket.on('chat', function(data) {
+    
+    output.innerHTML += '<p contenteditable="true" tabindex="' + x + '">' + data.name + '</p>';
+    var text = document.querySelectorAll('#output p');
+    
+    if (output.children.length > 4 & was) {
+        output.removeChild(output.children[0]);    
+    }
+    
+    for (let i = 0; i < text.length; i++ ) {
+        text[i].addEventListener('keyup', function(){
+
+
+            if (event.keyCode != 46) {
+                socket.emit('edit', {
+                    input: this.innerText,
+                    u_id: this.tabIndex
+                })            
+            } else{
+                
+                socket.emit('del', {
+                    del_id: this.tabIndex,
+                    
+                }) 
+
+            }
+        })    
+    }
+    x++;
+});
+
+var url = "../visitors.json";
+$.getJSON(url,
+    function(data){
+      console.log(data);
+    });
